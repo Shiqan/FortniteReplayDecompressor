c228db71e887cca3bdc3526a6a4d4cb4cb495ca9
Shiqan <ferron.saan@live.nl>
2019-02-20 19:25:42 +0100

    Initial commit

---
+++ src/ConsoleReader/Program.cs
@@ -0,0 +1,29 @@
+﻿using FortniteReplayReaderDecompressor;
+using System.IO;
+using static System.Environment;
+
+namespace ConsoleReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var localAppDataFolder = GetFolderPath(SpecialFolder.LocalApplicationData);
+            var replayFilesFolder = Path.Combine(localAppDataFolder, @"FortniteGame\Saved\Demos");
+
+            var replayFiles = Directory.EnumerateFiles(replayFilesFolder, "UnsavedReplay-2019.02.18-20.56.20.replay");
+
+            foreach (var replayFile in replayFiles)
+            {
+
+                using (var stream = File.Open(replayFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (var reader = new FortniteReplayDecompressor(stream))
+                    {
+                        var replay = reader.ReadFile();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/FortniteReplayDecompressor/FortniteReplayDecompressor.cs
@@ -0,0 +1,47 @@
+﻿using FortniteReplayReader;
+using System.IO;
+
+namespace FortniteReplayReaderDecompressor
+{
+    public class FortniteReplayDecompressor : FortniteBinaryReader
+    {
+        public FortniteReplayDecompressor(Stream input) : base(input)
+        {
+        }
+
+        public FortniteReplayDecompressor(Stream input, int offset) : base(input)
+        {
+
+        }
+        protected override void ParseCheckPoint()
+        {
+            var checkpointId = ReadFString();
+            var checkpoint = ReadFString();
+            using (var reader = Decompress())
+            {
+                reader.ReadUInt32();
+            }
+        }
+
+        protected override void ParseReplayData()
+        {
+            var start = ReadUInt32();
+            var end = ReadUInt32();
+            var length = ReadUInt32();
+            using (var reader = Decompress())
+            {
+                reader.ReadUInt32();
+            }
+        }
+
+        private FortniteBinaryReader Decompress()
+        {
+            var decompressedSize = ReadInt32();
+            var compressedSize = ReadInt32();
+            var compressedBuffer = ReadBytes(compressedSize);
+            var output = OodleBinding.DecompressReplayData(compressedBuffer, compressedBuffer.Length, decompressedSize);
+            File.WriteAllBytes("replaydata.dump", output);
+            return new FortniteBinaryReader(new MemoryStream(output));
+        }
+    }
+}

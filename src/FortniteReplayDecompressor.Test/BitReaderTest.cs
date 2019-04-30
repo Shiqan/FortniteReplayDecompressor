7b9f9c7584f0c246251cdc33c1d49ee1f9ac8770
Shiqan <ferron.saan@live.nl>
2019-04-30 19:08:56 +0200

    Add BitReader tests

---
+++ src/FortniteReplayDecompressor.Test/BitReaderTest.cs
@@ -0,0 +1,80 @@
+using FortniteReplayReaderDecompressor;
+using System;
+using Xunit;
+
+namespace FortniteReplayDecompressor.Test
+{
+    public class BitReaderTest
+    {
+        byte[] expectedBytes;
+        bool[] expectedBits;
+        BitReader reader;
+
+        public BitReaderTest()
+        {
+            expectedBits = new bool[] { true, true, false, false, false, true, false, false };
+            expectedBytes = new byte[] { 35, 47, 215 };
+            reader = new BitReader(expectedBytes);
+        }
+
+        [Fact]
+        public void ReadBitTest()
+        {            
+            for (var i = 0; i < expectedBits.Length; i++)
+            {
+                Assert.Equal(expectedBits[i], reader.ReadBit());
+                Assert.Equal(i + 1, reader.Position);
+            }
+            Assert.Equal(8, reader.Position);
+        }
+
+        [Fact]
+        public void ReadBitThrowsExceptionTest()
+        {
+            reader.Seek(24);
+            Assert.Throws<ArgumentOutOfRangeException>(() => reader.ReadBit());
+        }
+
+        [Fact]
+        public void PeekBitTest()
+        {
+            Assert.True(reader.PeekBit());
+            Assert.Equal(0, reader.Position);
+        }
+
+        [Fact]
+        public void SeekTest()
+        {
+            reader.Seek(8);
+            Assert.Equal(8, reader.Position);
+        }
+
+        [Fact]
+        public void SeekThrowsExceptionTest()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => reader.Seek(25));
+            Assert.Throws<ArgumentOutOfRangeException>(() => reader.Seek(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => reader.Seek(25, System.IO.SeekOrigin.End));
+
+            reader.Seek(20);
+            Assert.Throws<ArgumentOutOfRangeException>(() => reader.Seek(10, System.IO.SeekOrigin.Current));
+        }
+
+        [Fact]
+        public void ReadByteTest()
+        {
+            for (var i = 0; i < expectedBytes.Length; i++)
+            {
+                Assert.Equal(expectedBytes[i], reader.ReadByte());
+            }
+            Assert.Equal(expectedBytes.Length * 8, reader.Position);
+        }
+
+        [Fact]
+        public void ReadBytesTest()
+        {
+            Assert.Equal(expectedBytes, reader.ReadBytes(3));
+            Assert.Equal(expectedBytes.Length * 8, reader.Position);
+        }
+    }
+}
--- src/FortniteReplayDecompressor/BitReader.cs
+++ src/FortniteReplayDecompressor/BitReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
 
 namespace FortniteReplayReaderDecompressor
 {
@@ -10,7 +11,11 @@ namespace FortniteReplayReaderDecompressor
     public class BitReader
     {
         private readonly BitArray Bits;
-        private int Position;
+
+        /// <summary>
+        /// Position in current BitArray. Set with <see cref="Seek(int, SeekOrigin)"/>
+        /// </summary>
+        public int Position { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the BitReader class based on the specified bytes.
@@ -37,10 +42,66 @@ namespace FortniteReplayReaderDecompressor
         /// </summary>
         /// <returns>The value of the bit at position index.</returns>
         /// <seealso cref="PeekBit"/>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public virtual bool ReadBit()
         {
             return Bits[Position++];
         }
 
+        /// <summary>
+        /// Sets <see cref="Position"/> within current BitArray.
+        /// </summary>
+        /// <param name="offset">The offset relative to the <paramref name="seekOrigin"/>.</param>
+        /// <param name="seekOrigin">A value of type <see cref="SeekOrigin"/> indicating the reference point used to obtain the new position.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public virtual void Seek(int offset, SeekOrigin seekOrigin = SeekOrigin.Begin)
+        {
+            if (offset < 0 || offset > Bits.Length || (seekOrigin == SeekOrigin.Current && offset + Position > Bits.Length))
+            {
+                throw new ArgumentOutOfRangeException("Specified offset doesnt fit within the BitArray buffer");
+            }
+
+            _ = (seekOrigin switch
+            {
+                SeekOrigin.Begin => Position = offset,
+                SeekOrigin.End => Position = Bits.Length - offset,
+                SeekOrigin.Current => Position += offset,
+                _ => Position = offset,
+            });
+        }
+
+        /// <summary>
+        /// Returns the byte at <see cref="Position"/> and advances the <see cref="Position"/> by 8 bits.
+        /// </summary>
+        /// <returns>The value of the byte at <see cref="Position"/> index.</returns>
+        public virtual byte ReadByte()
+        {
+            var result = new byte();
+            for (int i = 0; i < 8; i++)
+            {
+                if (ReadBit())
+                {
+                    result |= (byte)(1 << i);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns <paramref name="bytes"/> bytes at <see cref="Position"/> and advances the <see cref="Position"/> by 8 bits.
+        /// </summary>
+        /// <returns>The value of the byte at <see cref="Position"/> index.</returns>
+        public virtual byte[] ReadBytes(int bytes)
+        {
+            var result = new byte[bytes];
+            for (int i = 0; i < bytes; i++)
+            {
+                result[i] = ReadByte();
+            }
+            return result;
+        }
     }
+
 }

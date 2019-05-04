d723f7698d3df91ca8100501f484ce4f8699cc46
Shiqan <ferron.saan@live.nl>
2019-05-04 09:41:56 +0200

    Actually add .replay files as well...

--- src/ConsoleReader/Program.cs
+++ src/ConsoleReader/Program.cs
@@ -8,8 +8,9 @@ namespace ConsoleReader
     {
         static void Main(string[] args)
         {
-            //var replayFile = "Replays/UnsavedReplay-2018.10.06-22.00.32.replay";
-            var replayFile = "Replays/UnsavedReplay-2019.05.03-21.24.46.replay";
+            var replayFile = "Replays/UnsavedReplay-2018.10.06-22.00.32.replay";
+            //var replayFile = "Replays/UnsavedReplay-2019.04.05-20.22.58.replay";
+            //var replayFile = "Replays/UnsavedReplay-2019.05.03-21.24.46.replay";
             using (var stream = File.Open(replayFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
                 using (var reader = new FortniteBinaryDecompressor(stream))

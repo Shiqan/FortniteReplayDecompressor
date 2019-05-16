182976d3800ef49aa8d440fa181256d1456ce07f
Shiqan <ferron.saan@live.nl>
2019-05-16 22:19:55 +0200

    Add Packet reading test(s)

---
+++ src/FortniteReplayDecompressor.Test/PacketTest.cs
@@ -0,0 +1,44 @@
+﻿using FortniteReplayReader.Core.Models;
+using FortniteReplayReader.Core.Models.Enums;
+using FortniteReplayReaderDecompressor;
+using FortniteReplayReaderDecompressor.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace FortniteReplayDecompressor.Test
+{
+    public class PacketTest
+    {
+        [Fact]
+        public void ProcessPacketTest()
+        {
+            var packet = @"Packets/packet0.dump";
+            using (var stream = File.Open(packet, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                var replay = new Replay()
+                {
+                    Header = new Header()
+                    {
+                        EngineNetworkVersionHistory = EngineNetworkVersionHistory.HISTORY_NEW_ACTOR_OVERRIDE_LEVEL
+                    }
+                };
+
+                using (var reader = new FortniteBinaryDecompressor(stream, replay))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        stream.CopyTo(ms);
+                        var playbackPacket = new PlaybackPacket()
+                        {
+                            Data = ms.ToArray()
+                        };
+                        reader.ProcessPacket(playbackPacket);
+                    }                   
+                }
+            }
+        }
+    }
+}

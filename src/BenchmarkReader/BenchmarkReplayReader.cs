e53a2b29dd442d8cb146a277498fea57a9088429
Shiqan <ferron.saan@live.nl>
2020-02-22 09:55:56 +0100

    tests for replay header + replay info

--- src/BenchmarkReader/BenchmarkReplayReader.cs
+++ src/BenchmarkReader/BenchmarkReplayReader.cs
@@ -18,6 +18,7 @@ namespace BenchmarkReader
         [Arguments("Replays/UnsavedReplay-2019.09.12-21.39.37.replay", ParseMode.Normal)]
         [Arguments("Replays/season6.10.replay", ParseMode.Normal)]
         [Arguments("Replays/season11.replay", ParseMode.Normal)]
+        [Arguments("Replays/season12.replay", ParseMode.Normal)]
         public new FortniteReplay ReadReplay(string fileName, ParseMode mode = ParseMode.Minimal)
         {
             using var stream = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
